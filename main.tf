4fd7a750c6427dab4aca0d483c910406456024d3
Thomas Lopez <tomoss18@yahoo.com>
2023-05-31 11:02:16 -0500

    updated tags on igw

--- main.tf
+++ main.tf
@@ -11,6 +11,9 @@ resource "aws_vpc" "main" {
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "tlopez-igw"
+  }
 }
 
 resource "aws_subnet" "pubsub1" {
