3c7a62f91ad2d0dc798ebe81af198c2febfae7ed
Thomas Lopez <tomoss18@yahoo.com>
2023-05-31 10:52:33 -0500

    route table association

--- main.tf
+++ main.tf
@@ -48,3 +48,8 @@ resource "aws_route" "public" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.main.id
 }
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = [aws_subtnet.pubsub1.id, aws_subnet.pubsub2.id]
+}
