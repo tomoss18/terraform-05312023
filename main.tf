7d564477844d69d13bcb13ccb5a5ce3146e5402f
Thomas Lopez <tomoss18@yahoo.com>
2023-05-31 10:39:20 -0500

    2 public subnets created

--- main.tf
+++ main.tf
@@ -12,3 +12,25 @@ resource "aws_vpc" "main" {
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_subnet" "pubsub1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_customer_owned_ip_on_launch = true
+  tags = {
+    Name = "tlopez-pubsub1"
+    env  = "Dev"
+  }
+}
+
+resource "aws_subnet" "pubsub2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1b"
+  map_customer_owned_ip_on_launch = true
+  tags = {
+    Name = "tlopez-pubsub2"
+    env  = "Dev"
+  }
+}
