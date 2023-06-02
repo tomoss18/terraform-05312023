205c96b6a81c7d587ee18c23bba9418345a80ecc
Thomas Lopez <tomoss18@yahoo.com>
2023-06-02 08:40:17 -0500

    endpoints

--- main.tf
+++ main.tf
@@ -9,6 +9,30 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.ssm"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ssmmessages" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.ssmmessages"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.ec2"
+  vpc_endpoint_type = "Interface"
+}
+
+resource "aws_vpc_endpoint" "ec2messages" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.us-east-1.ec2messages"
+  vpc_endpoint_type = "Interface"
+}
+
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
   tags = {
